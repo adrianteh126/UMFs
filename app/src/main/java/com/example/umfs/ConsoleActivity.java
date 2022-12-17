bc395d1411405d486fd7df26fa819b900d37f513
sorfina27 <u2002117@siswa.um.edu.my>
2022-12-17 18:55:51 +0800

    Activities and Fragments

---
+++ app/src/main/java/com/example/umfs/ConsoleActivity.java
@@ -0,0 +1,14 @@
+package com.example.umfs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_console);
+    }
+}
