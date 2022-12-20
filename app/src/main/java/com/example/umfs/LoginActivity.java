b86a034ce1a0aca40d74e1c61f3167cf8daf90e3
sorfina27 <u2002117@siswa.um.edu.my>
2022-12-20 11:23:52 +0800

    Register

--- app/src/main/java/com/example/umfs/LoginActivity.java
+++ app/src/main/java/com/example/umfs/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.umfs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button BTRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BTRegister = findViewById(R.id.BTRegister);
+
+        BTRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/umfs/MainActivity.java
+++ app/src/main/java/com/example/umfs/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.umfs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Start = findViewById(R.id.BTStart);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
