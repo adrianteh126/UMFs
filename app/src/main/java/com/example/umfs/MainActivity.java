8840e5cbb542243558fb553fd99f7a772f5272f6
adrianteh126 <91460516+adrianteh126@users.noreply.github.com>
2022-12-26 01:58:37 +0800

    25/12/2022 Appbar with navigation to notification and search (#4)
    * Test pushing a layout file to keat fork 2
    
    * Delete test_keat.xml file
    
    * 25/12/2022
    Update:
    - add appBar with navigation to notification fragment and search view[activity_main.xml]
    - add new color value, set default primary color
    - Search
    - Post
    - MainActivity -> Set the default landing page to fragment_posts.xml
    Create :
    - activity_search.xml
    - upload some svg files(icons)
    - Notification Fragment
    - Implement menu(bottom, side, search)
    - Implement 2 nav graph (umfs, search)
    Modify :
    - modified gradle file (details in the comment)

--- app/src/main/java/com/example/umfs/MainActivity.java
+++ app/src/main/java/com/example/umfs/MainActivity.java
@@ -1,28 +1,73 @@
 package com.example.umfs;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button Start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Start = findViewById(R.id.BTStart);
 
-        Start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-            }
-        });
+        //setup action bar
+        Toolbar toolbar = findViewById(R.id.TBMainAct);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.DLMain);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this
+                , drawerLayout,toolbar
+                ,R.string.navigation_drawer_open
+                ,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        //Make the navigation drawer icon always appear on the corner
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // setup navigation graph (menu)
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFMain);
+        NavController navController = host.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        setupNavMenu(navController);
     }
+
+    //setupBottomNavMenu(navController) method
+    private void setupNavMenu (NavController navController) {
+        NavigationView sideNav = findViewById(R.id.sideNav);
+        NavigationUI.setupWithNavController(sideNav,navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_side,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        try {
+            Navigation.findNavController(this,R.id.NHFMain).navigate(item.getItemId());
+            return true;
+        }
+        catch (Exception ex) {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/com/example/umfs/SearchActivity.java
+++ app/src/main/java/com/example/umfs/SearchActivity.java
@@ -1,8 +1,16 @@
 package com.example.umfs;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +18,44 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+//        //setup action bar (disabled - should not have action bar)
+//        Toolbar toolbar = findViewById(R.id.TBMainAct);
+//        setSupportActionBar(toolbar);
+//        DrawerLayout drawerLayout = findViewById(R.id.DLMain);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this
+//                , drawerLayout,toolbar
+//                ,R.string.navigation_drawer_open
+//                ,R.string.navigation_drawer_close);
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.menu_search,menu);
+//        MenuItem menuItem = menu.findItem(R.id.app_bar_search);
+//        SearchView searchView = (SearchView) menuItem.getActionView();
+////        The Query Hint can be change later
+//        searchView.setQueryHint("Trending topic : World Cup 2022");
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+////            TODO: Implement the search function here
+//// https://www.youtube.com/watch?v=M3UDh9mwBd8&ab_channel=Foxandroid
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                SearchLandingFragment landingFragment = null;
+//                landingFragment.getArrayAdapter().getFilter().filter(newText);
+//                return false;
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
 }
