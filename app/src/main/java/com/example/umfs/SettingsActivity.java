caa733724826c92ca88dc4315fb3c185d1429081
sorfina27 <u2002117@siswa.um.edu.my>
2023-01-02 20:13:01 +0800

    Forgot Password, Settings Activity + Layout Created

--- app/src/main/java/com/example/umfs/SettingsActivity.java
+++ app/src/main/java/com/example/umfs/SettingsActivity.java
@@ -1,33 +1,19 @@
 package com.example.umfs;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Button BTLogOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.settings, new SettingsFragment())
-                    .commit();
-        }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
-}
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_settings);
+        };
+    };
