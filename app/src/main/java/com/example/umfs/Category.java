81d51cc39a66a5f3302808d36e2886daf44c5f04
Kuan Kiat <adrianteh02@hotmail.com>
2023-01-03 17:18:28 +0800

    3/1/2023 update: - Link categories to firebase

---
+++ app/src/main/java/com/example/umfs/Category.java
@@ -0,0 +1,37 @@
+package com.example.umfs;
+
+public class Category {
+
+    private String category;
+    private boolean isTrendingNow;
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    public Category(String category, boolean isTrendingNow) {
+        this.category = category;
+        this.isTrendingNow = isTrendingNow;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isTrendingNow() {
+        return isTrendingNow;
+    }
+
+    public void setTrendingNow(boolean trendingNow) {
+        isTrendingNow = trendingNow;
+    }
+
+    @Override
+    public String toString() {
+        return this.category;
+    }
+}
