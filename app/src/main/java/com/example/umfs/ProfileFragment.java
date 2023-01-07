1831099eea0967af175fb87acc7115a9b12a372c
sorfina27 <u2002117@siswa.um.edu.my>
2023-01-07 12:09:57 +0800

    Setup Profile update

--- app/src/main/java/com/example/umfs/ProfileFragment.java
+++ app/src/main/java/com/example/umfs/ProfileFragment.java
@@ -23,6 +23,7 @@ public class ProfileFragment extends Fragment {
 
     Activity context;
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
