01f1e2c812040c7134744c1971324dc6f99156e0
tiansien <tiansien02@gmail.com>
2023-01-11 22:50:36 +0800

    all about message

--- app/src/main/java/com/example/umfs/MessageListFragment.java
+++ app/src/main/java/com/example/umfs/MessageListFragment.java
@@ -1,12 +1,27 @@
 package com.example.umfs;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+
+import com.example.umfs.databinding.ActivityChatBinding;
+import com.example.umfs.databinding.FragmentHomeBinding;
+import com.example.umfs.databinding.FragmentMessageListBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MessageListFragment extends Fragment {
+
+    FragmentMessageListBinding binding;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private ChatAdapter chatAdapter;
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +80,44 @@ public class MessageListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message_list, container, false);
+
+        binding  = FragmentMessageListBinding.inflate(inflater, container, false);
+        binding= FragmentMessageListBinding.inflate(getLayoutInflater());
+        mAuth= FirebaseAuth.getInstance();
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+        chatAdapter=new ChatAdapter(getContext());
+        binding.messageRecycleView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ImageView chat_back = binding.chatBack;
+        chat_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                chatAdapter.clear();
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    String uid=dataSnapshot.getKey();
+                    if (!uid.equals(FirebaseAuth.getInstance().getUid())){
+                        User user=dataSnapshot.getValue(User.class);
+                        chatAdapter.add(user);
+                    }
+                }
+                binding.messageRecycleView.setAdapter(chatAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return binding.getRoot();
     }
 }
