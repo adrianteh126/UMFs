ad15edda4129f0c2cd0d480e47cff3f106171979
sorfina27 <u2002117@siswa.um.edu.my>
2023-01-13 12:47:47 +0800

    User Account Final Changes

--- app/src/main/java/com/example/umfs/LoginActivity.java
+++ app/src/main/java/com/example/umfs/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.example.umfs;
 
-import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,6 +18,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,6 +35,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText ETSiswamail;
     private EditText ETPassword;
     private FirebaseAuth auth;  //for sign-in authentication
+    private FirebaseDatabase databaseRoot;  //access to UMFs realtime database @ firebase
+    private DatabaseReference reference;    //reference to node under UMFs database aka Users node
+    private String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,9 @@ public class LoginActivity extends AppCompatActivity {
 
         ETSiswamail.addTextChangedListener(TWLogin);
         ETPassword.addTextChangedListener(TWLogin);
+        databaseRoot = FirebaseDatabase.getInstance();
+        //reference to Users node under UMFs realtime database
+        reference = databaseRoot.getReference().child("Users");
 
         BTLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,6 +123,28 @@ public class LoginActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     //store user that is logged in, so they don't have to log in again
                     FirebaseUser user = auth.getCurrentUser();
+                    currentUserID = user.getUid();
+                    DatabaseReference userRef = reference.child(currentUserID);
+                    try {
+                        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                User user = snapshot.getValue(User.class);
+                                if (!user.getPassword().equals(txt_password)) {
+                                    Map<String, Object> passwordUpdate = new HashMap<>();
+                                    passwordUpdate.put("password", txt_password);
+                                    userRef.updateChildren(passwordUpdate);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }catch (Exception e){
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
                     Intent intent = new Intent();
                     Toast.makeText(LoginActivity.this, "Welcome to UMFs", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
