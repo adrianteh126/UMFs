46b859d3683e6a528aa2ca2c9db98b9dab55ddcd
JessonLawCongJi <u2102851@siswa.um.edu.my>
2023-01-19 17:08:25 +0800

    UI changed, Delete function needs help
    Authentication can be deleted, realtime database related to post cannot be deleted

--- app/src/main/java/com/example/umfs/SettingsActivity.java
+++ app/src/main/java/com/example/umfs/SettingsActivity.java
@@ -7,16 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsActivity extends AppCompatActivity {
     private Button BTLogOut;
+    private Button BTDeleteProfile;
     private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         mAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         BTLogOut = findViewById(R.id.BTLogOut);
 
         BTLogOut.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +42,52 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        BTDeleteProfile = findViewById(R.id.BTDeleteProfile);
+        BTDeleteProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmDelete();
+            }
+        });
+
+
+    }
+
+    private void confirmDelete() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm DELETE Account?")
+                .setMessage("Deleting this account will result in completely removing your account from the system")
+                .setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(SettingsActivity.this, "Your account has been deleted.", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                            Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(SettingsActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+                        reference.child(firebaseUser.getUid()).removeValue();
+                        /**
+                        DatabaseReference postReference = FirebaseDatabase.getInstance().getReference().child("posts");
+                         */
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
     }
 
     public void closeApp(){
